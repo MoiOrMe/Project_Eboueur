8b47c5de15001ffde73d0a3b3d7a318220ea2f38
MoiOrMe <robin.rivard@efrei.net>
2025-06-13 14:10:00 +0200

    résolution bug I hope

--- Assets/Scripts/AideVisuelleManager.cs
+++ Assets/Scripts/AideVisuelleManager.cs
@@ -11,7 +11,7 @@ public class AideVisuelleManager : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject); // Optionnel si tu changes de scènes
+            DontDestroyOnLoad(gameObject);
         }
         else
         {
